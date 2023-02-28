a1a0d328d4e48cfb020694732663d54611cc59e8
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-02-28 18:23:54 +0300

    lsd sort

---
+++ lsd_sort.h
@@ -0,0 +1,12 @@
+#ifndef LSD_SORT_H
+#define LSD_SORT_H
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+void lsd_sort (uint32_t *arr, int size);
+void swap_uint32 (uint32_t *val1, uint32_t *val2);
+
+#endif /* LSD_SORT_H */
